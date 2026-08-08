69493d65953221decfd9a7039b95ad2a02c07576
subhradeep-maji <subhradipm59@gmial.com>
2026-08-08 17:24:06 +0530

    feat: update package.json and package-lock.json to include @types/react and @types/react-dom; add global.d.ts for module declarations; modify main.tsx for improved type support

---
+++ services/client/src/global.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-dom/client';
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+declare module '*.styl';
--- services/client/src/main.tsx
+++ services/client/src/main.tsx
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
